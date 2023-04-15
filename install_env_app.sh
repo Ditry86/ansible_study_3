cf279eeedc066d1ae1ff3ecce5a61996145766c8
ditry86 <suetin.dmitry@gmail.com>
2023-04-15 12:17:46 +0900

    change install_env_app

--- install_env_app.sh
+++ install_env_app.sh
@@ -19,10 +19,10 @@ then
     read -s $passwd
     case $distro in
         ubuntu | Ubuntu )
-            echo $passwd | sudo apt update && sudo apt upgrade -y
+            echo $passwd | sudo apt update && sudo apt upgrade -y > /dev/null
             ;;
         centos | Centos | CentOs | CentOS)
-            echo $passwd | sudo yum update && sudo yum upgrade -y
+            echo $passwd | sudo yum update && sudo yum upgrade -y > /dev/null
             ;;
     esac
     mkdir $cur_dir/tmp
@@ -42,14 +42,14 @@ if  [ $tf != 0 ]; then
     echo $'\n'Installed Terraform...$'\n'==============================================================$'\n'
     case $distro in
         ubuntu | Ubuntu )
-            echo $passwd | sudo apt install -y unzip
+            echo $passwd | sudo apt install -y unzip > /dev/null
             ;;
         centos | Centos | CentOs | CentOS)
-            echo $passwd | sudo yum install -y unzip
+            echo $passwd | sudo yum install -y unzip > /dev/null
             ;;
     esac
     curl -L -k https://hashicorp-releases.yandexcloud.net/terraform/1.4.2/terraform_1.4.2_linux_amd64.zip > terraform.zip
-    echo passwd | sudo unzip -d /usr/local/bin terraform.zip
+    echo $passwd | sudo unzip -d /usr/local/bin terraform.zip
     echo --------------------------------------------------------------$'\n'Done!$'\n'
 fi
 #Install python3
@@ -57,10 +57,10 @@ if  [ $py != 0 ]; then
     echo $'\n'Installed Python3...$'\n'==============================================================$'\n'
     case $distro in
         ubuntu | Ubuntu )
-            echo $passwd | sudo apt install -y python3 python3-dev
+            echo $passwd | sudo apt install -y python3 python3-dev > /dev/null
             ;;
         centos | Centos | CentOs | CentOS)
-            echo $passwd | sudo yum install -y python3 python3-devel
+            echo $passwd | sudo yum install -y python3 python3-devel > /dev/null
             ;;
     esac
     echo --------------------------------------------------------------$'\n'Done!$'\n'
@@ -70,11 +70,11 @@ if  [ $an != 0 ]; then
     echo $'\n'Installed Ansible '(by using pip)'...$'\n'==============================================================$'\n'
     which pip3 > /dev/null
     if [ "$distro" == "ubuntu" ] || [ "$distro" == "Ubuntu" ]; then
-        echo $passwd | sudo apt install python3-pip
+        echo $passwd | sudo apt install python3-pip > /dev/null
     fi
-    echo $passwd | sudo python3 -m pip install --upgrade pip
-    echo $passwd | sudo python3 -m pip install --user netaddr
-    python3 -m pip install --upgrade --user ansible
+    echo $passwd | sudo python3 -m pip install --upgrade pip > /dev/null
+    echo $passwd | sudo python3 -m pip install --user netaddr > /dev/null
+    python3 -m pip install --upgrade --user ansible > /dev/null
     echo $HOME
     echo $passwd | sudo echo 'export PATH=$PATH:~/.local/bin' >> ~/.bashrc
     echo --------------------------------------------------------------$'\n'Done!$'\n'
