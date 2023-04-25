b5c9f02cf6f7727cce061e4e9a8d8b03f898e276
ditry86 <suetin.dmitry@gmail.com>
2023-04-25 16:15:39 +0900

    fix all

--- get_ext_ip.sh
+++ get_ext_ip.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd terraform
-export CLICKHOUSE_IP=$(terraform output | grep clickhouse | sed 's/\s*"clickhouse" = "//;s/"$//')
-export VECTOR_IP=$(terraform output | grep vector | sed 's/\s*"vector" = "//;s/"$//')
-export LIGHTHOUSE_IP=$(terraform output | grep lighthouse | sed 's/\s*"lighthouse" = "//;s/"$//')
+export CLICKHOUSE_IP=$(terraform output | grep clickhouse | sed 's/\s*"clickhouse" = "//;s/"$//') >> ../ext_ip
+export VECTOR_IP=$(terraform output | grep vector | sed 's/\s*"vector" = "//;s/"$//') >> ../ext_ip
+export LIGHTHOUSE_IP=$(terraform output | grep lighthouse | sed 's/\s*"lighthouse" = "//;s/"$//') >> ../ext_ip
 cd ..
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,16 +1,4 @@
-variable "yc_folder_id" {
-    default=""
-}
-
-variable "yc_token" {
-    default=""
-}
-
-variable "yc_cloud_id" {
-    default=""
-}
-
-variable "yc_zone" {
+variable "zone" {
     default=""
 }
 
